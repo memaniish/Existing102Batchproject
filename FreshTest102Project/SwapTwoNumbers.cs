9a8e0c4d0db4a7512ba183a7f568bce51d9f166a
Manish <krmanish2197@gmail.com>
2022-01-14 12:18:02 +0530

    First commit

---
+++ FreshTest102Project/SwapTwoNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshTest102Project
+{
+    class SwapTwoNumbers
+    {
+        public static void SwapingNumbers()
+        {
+            //camel case for local variables and method parameters
+            
+            int firstNum, secondNum, temp;
+            Console.WriteLine("Please enter the first number");
+            firstNum=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            secondNum=Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("Before exchanging values: first={0},second={1}", firstNum,secondNum);
+            temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+            Console.WriteLine("After exchanging values: first={0},second={1}", firstNum,secondNum);
+
+
+        }
+    }
+}
